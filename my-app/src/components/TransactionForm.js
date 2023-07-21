b0c106da936e58255d2b51403cb4a46c581f23c8
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-22 00:40:14 +0300

    Mission Passed

--- my-app/src/components/TransactionForm.js
+++ my-app/src/components/TransactionForm.js
@@ -1,81 +1,74 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const TransactionForm = ({onAddTransaction}) => {
-    const [date, setDate] = useState('');
-    const [description, setDescription] = useState('');
-    const [category, setCategory] = useState('');
-    const [amount, setAmount] = useState('');
+const TransactionForm = ({ onAddTransaction }) => {
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
 
-
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-const newTransaction = {
-    date,
-    description,
-    category,
-    amount:parseFloat(amount),
-};
+    const newTransaction = {
+      date,
+      description,
+      category,
+      amount: parseFloat(amount),
+    };
 
-onAddTransaction(newTransaction);
+    onAddTransaction(newTransaction);
 
-setDate('');
-setDescription('');
-setCategory('');
-setAmount('');
+    setDate("");
+    setDescription("");
+    setCategory("");
+    setAmount("");
+  };
 
-};
-
-
-return (
+  return (
     <form onSubmit={handleSubmit}>
-     <div>
+      <div>
         <label htmlFor="date">Date:</label>
         <input
-         type="date"
-         id="date"
-         value={date}
-         onChange={(e) => setDate(e.target.value)}
-         required
+          type="date"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
         />
-     </div>
-     <div>
+      </div>
+      <div>
         <label htmlFor="description">Description:</label>
         <input
-         type="text"
-         id="description"
-         value={description}
-         onChange={(e) => setDescription(e.target.value)}
-         required
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
         />
-     </div>
-     <div>
+      </div>
+      <div>
         <label htmlFor="category">Category:</label>
         <input
-         type="text"
-         id="category"
-         value={category}
-         onChange={(e) => setCategory(e.target.value)}
-        required
+          type="text"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
         />
-     </div>
-     <div>
+      </div>
+      <div>
         <label htmlFor="amount">Amount:</label>
         <input
-         type="number"
-         id="amount"
-         value={amount}
-         onChange={(e) => setAmount(e.target.value)}
-        required
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
         />
-     </div>
-     <button type="submit">Add Transaction</button>
+      </div>
+      <button type="submit">Add Transaction</button>
     </form>
-);
-
+  );
 };
 
 export default TransactionForm;
-
-
-
