512be85e014c040f5596a5f3cdf5ee098941df6b
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-22 15:47:13 +0300

    Mission Passed

--- my-app/src/components/TransactionTable.js
+++ my-app/src/components/TransactionTable.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 const TransactionTable = ({transactions}) => {
-    
+    return (
+     <table>
+       <thead>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th> 
+        </tr>
+       </thead>
+       <tbody>
+        {transactions.map((transactions) => 
+        <tr key={transactions.id}>
+         <td>{transactions.date}</td>
+         <td>{transactions.description}</td>
+         <td>{transactions.category}</td>
+         <td>{transactions.amount}</td>
+        </tr>
+        )}
+       </tbody>
+
+     </table>
+
+    )
 }
