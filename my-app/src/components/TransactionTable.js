d84a73288508226f3949eb87ca5479419bd1d1f4
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-22 15:48:56 +0300

    Mission Passed

--- my-app/src/components/TransactionTable.js
+++ my-app/src/components/TransactionTable.js
@@ -1,32 +1,28 @@
 import React from "react";
 
-const TransactionTable = ({transactions}) => {
-    return (
-     <table>
-       <thead>
+const TransactionTable = ({ transactions }) => {
+  return (
+    <table>
+      <thead>
         <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Amount</th> 
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
         </tr>
-       </thead>
-       <tbody>
+      </thead>
+      <tbody>
         {transactions.map((transactions) => (
-        <tr key={transactions.id}>
-         <td>{transactions.date}</td>
-         <td>{transactions.description}</td>
-         <td>{transactions.category}</td>
-         <td>{transactions.amount}</td>
-        </tr>
-        )
-        )}
-        
-       </tbody>
-
-     </table>
-
-    )
-}
+          <tr key={transactions.id}>
+            <td>{transactions.date}</td>
+            <td>{transactions.description}</td>
+            <td>{transactions.category}</td>
+            <td>{transactions.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default TransactionTable;
